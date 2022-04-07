1ab6f723093c2ed46d68453783c3102ddad496a6
IceDiver <admin@example.com>
2022-04-07 11:59:30 +0300

    add useReducer

--- src/components/temp/App_141_useReducer.js
+++ src/components/temp/App_141_useReducer.js
@@ -1,10 +1,33 @@
-import {useState} from 'react';
+import {useState, useReducer} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-const Slider = () => {
+function reducer(state, action) {
+    switch (action.type) {
+        case 'toggle':
+            return {autoplay: !state.autoplay};
+            
+        case 'slow':
+            return {autoplay: 300};
+            
+        case 'fast':
+            return {autoplay: 700};
+        case 'custom':
+            return {autoplay: action.payload};
+        default:
+            throw new Error();
+            
+    }
+}
+function init (initial) {
+    return {
+        autoplay: initial
+    }
+}
+const Slider = ({initial}) => {
     const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoplay] = useState(false);
+    // const [autoplay, setAutoplay] = useState(false);
+    const [autoplay, dispatch] = useReducer(reducer, initial, init);
 
     function changeSlide(i) {
         setSlide(slide => slide + i);
@@ -24,7 +47,16 @@ const Slider = () => {
                         onClick={() => changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => setAutoplay(!autoplay)}>toggle autoplay</button>
+                        onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'fast'})}>fast autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={(e) => dispatch({type: 'custom', payload: +e.target.textContent})}>1000</button>
                 </div>
             </div>
         </Container>
@@ -33,7 +65,7 @@ const Slider = () => {
 
 function App() {
     return (
-        <Slider/>
+        <Slider initial={false}/>
     );
 }
 
